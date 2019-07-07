edd4464991c99c1baf94cb005d8be8deb3636da3
maquetSDU <43057203+maquetSDU@users.noreply.github.com>
2019-07-07 09:39:49 +0600

    Add files via upload

--- db.php
+++ db.php
@@ -6,3 +6,11 @@ $user = 'birjeti';
 $pass = 'password';
 $charset = 'utf8';
 
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+# Подключение
+$pdo = new PDO($dsn, $user, $pass, $opt);
